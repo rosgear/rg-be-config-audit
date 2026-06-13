8877333b8d10ce5abb20a12ca57b28ef06ccdca5
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-13 22:36:42 +0300

    Файл конфигурации версии расширения

---
+++ config/.version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * Файл конфигурации версии расширения.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'name'        => 'Audit settings',
+    'description' => 'Documentation of operations performed by users',
+    'version'     => '1.0',
+    'versionDate' => '27-04-2025',
+    'author'      => 'RosGear',
+    'authorUrl'   => 'https://rosgear.ru/',
+    'email'       => 'info@rosgear.ru',
+    'url'         => 'https://rosgear.ru/component/rg-be-config.audit/',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://rosgear.ru/license/#license-mit'
+];
