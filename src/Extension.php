cb76a074a0a2d016e8d18274401eb0fe8d6b80d2
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-13 22:35:22 +0300

    Extension

---
+++ src/Extension.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Расширение модуля веб-приложения RosGear.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+namespace Rg\Backend\Config\Audit;
+
+/**
+ * Расширение "Аудит".
+ * 
+ * Настройка аудита.
+ * 
+ * Расширение принадлежит модулю "Конфигурация".
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Rg\Backend\Config\Audit
+ * @since 1.0
+ */
+class Extension extends \Ge\Panel\Extension\Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public string $id = 'rg.be.config.audit';
+
+    /**
+     * {@inheritdoc}
+     */
+    public string $defaultController = 'form';
+}
